349e0d9f2cc6dd80257c049e3accd21dd47eb6ec
moamen2020 <m01144944981@gmail.com>
2020-10-27 11:01:37 +0200

    Updates

---
+++ precache-manifest.74f05c11bbccce55501c28e78ed3274d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9785b7154c741caf244d730de1a40d",
+    "url": "/template_react/index.html"
+  },
+  {
+    "revision": "a4c5a1d83e45536ab4f6",
+    "url": "/template_react/static/css/main.57c6eb79.chunk.css"
+  },
+  {
+    "revision": "b8cae054d41c2220f1b1",
+    "url": "/template_react/static/js/2.ad1f0ff6.chunk.js"
+  },
+  {
+    "revision": "a4c5a1d83e45536ab4f6",
+    "url": "/template_react/static/js/main.a3e75dde.chunk.js"
+  },
+  {
+    "revision": "057475b267d8b0996434",
+    "url": "/template_react/static/js/runtime-main.54e85250.js"
+  }
+]);
